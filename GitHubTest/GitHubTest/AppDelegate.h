455e8ffb443d3dd1a92e7081e37b18e231e39007
zj <zhangjingchn@sina.cn>
2015-12-01 10:05:58 +0800

    testGitHub

---
+++ GitHubTest/GitHubTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubTest
+//
+//  Created by tarena04_01 on 15/12/1.
+//  Copyright © 2015年 tarena04_01. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
